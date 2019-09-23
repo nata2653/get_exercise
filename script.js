98a59fd3f1bf6c203986cf8af8bcc6360a019dbc
Natalie Sirich <nata2653@stud.kea.dk>
2019-09-23 11:40:17 +0200

    add exercise 2

--- script.js
+++ script.js
@@ -9,15 +9,46 @@ function get() {
   })
     .then(elm => elm.json())
     .then(movies => {
-      movies.forEach(movie => {
-        const template = document.querySelector("template").content;
-        const copy = template.cloneNode(true);
-        copy.querySelector("h1").textContent = movie.name;
-        copy.querySelector("h2").textContent = movie.year;
-        copy.querySelector("p").textContent = movie.protagonist;
-        document.querySelector("#app").appendChild(copy);
-      });
+      movies.forEach(addNewMovie);
     });
 }
 
 get();
+
+function addNewMovie(movie) {
+  const template = document.querySelector("template").content;
+  const copy = template.cloneNode(true);
+  copy.querySelector("h1").textContent = movie.name;
+  copy.querySelector("h2").textContent = movie.year;
+  copy.querySelector("p").textContent = movie.protagonist;
+  document.querySelector("#app").appendChild(copy);
+}
+
+function post() {
+  const movie = {
+    name: "Limitless",
+    protagonist: "santa",
+    year: 2018
+  };
+
+  const postData = JSON.stringify(movie);
+  console.log(postData);
+  fetch("https://frontend2019-28cd.restdb.io/rest/movies", {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      "x-apikey": "5d887478fd86cb75861e25fe",
+      "cache-control": "no-cache"
+    },
+    body: postData
+  })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      addNewMovie(data);
+    });
+}
+
+document.querySelector("button").addEventListener("click", elm => {
+  post();
+});
